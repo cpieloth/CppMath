073b62df961bfd450a798fcdac6ee4400e5a9f32
Christof Pieloth <cpieloth@gmx.net>
2014-12-19 15:25:09 +0100

    [ADD] Use Packbacker for dependencies.

---
+++ tools/PackBacker/jobs/CppMath_dependencies.pb
@@ -0,0 +1,3 @@
+# CppMath dependencies
+cxxtest: dest_dir=~/CppMath_dependencies;
+eigen3: dest_dir=~/CppMath_dependencies;
---
+++ tools/install_dependencies.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "CppMath requires:"
+echo "- Eigen for basic linear algebra routines"
+echo "- CxxTest for unit testing (optional)"
+echo ""
+echo "The destination folder is:"
+echo $HOME"/CppMath_dependencies/"
+echo ""
+echo "To change it, you can modify:"
+echo `pwd`"/PackBacker/jobs/CppMath_dependencies.pb"
+echo ""
+echo "If your distribution does not contain the following packages,"
+echo "you have to install them:"
+echo "- Eigen3"
+echo "- CxxTest"
+echo ""
+python3 ./PackBacker/packbacker.py ./PackBacker/jobs/CppMath_dependencies.pb
